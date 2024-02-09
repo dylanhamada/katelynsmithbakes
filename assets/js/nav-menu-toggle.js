9421103db8872ab3c22c1abfc11997443ddc44f2
Dylan Hamada <dylan.hamada@gmail.com>
2024-02-09 14:41:00 -0500

    Fix JavaScript Toggle and Responsive Navigation Menu
    - Fix menu and close button toggle in nav-menu-toggle.js
    - Fix sliding animation
    - Fix background color

--- assets/js/nav-menu-toggle.js
+++ assets/js/nav-menu-toggle.js
@@ -1,22 +1,17 @@
 const menuButton = document.querySelector(".nav-mobile-menu");
 const mobileMenu = document.querySelector(".nav-responsive");
+let closeButton = false;
 
 menuButton.addEventListener("click", () => {
-  const computedStyle = window.getComputedStyle(mobileMenu);
-  const newImagePath = "{{ site.baseurl }}/assets/icons/";
-  let closeButton = false;
-
-  if (computedStyle.display === "none") {
-    mobileMenu.style.display = "block";
-  } else {
-    mobileMenu.style.display = "none";
-  }
+  mobileMenu.classList.toggle("show");
 
   if (closeButton === false) {
-    menuButton.src = `${newImagePath}x-lg.svg`;
-    !closeButton;
+    menuButton.classList.remove("menu-open");
+    menuButton.classList.add("menu-closed")
+    closeButton = true;
   } else {
-    menuButton.src = `${newImagePath}list.svg`;
-    !closeButton;
+    menuButton.classList.remove("menu-closed")
+    menuButton.classList.add("menu-open");
+    closeButton = false;
   }
 });
