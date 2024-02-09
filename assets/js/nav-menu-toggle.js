e5b33481e0a003967e71b40c7d4346c1a0858dae
Dylan Hamada <dylan.hamada@gmail.com>
2024-02-09 12:56:28 -0500

    JavaScript Toggle for Mobile Navigation Menu
    - Add event listener on mobile nav menu button click

--- assets/js/nav-menu-toggle.js
+++ assets/js/nav-menu-toggle.js
@@ -1,3 +1,22 @@
 const menuButton = document.querySelector(".nav-mobile-menu");
+const mobileMenu = document.querySelector(".nav-responsive");
 
-console.log(menuButton);
+menuButton.addEventListener("click", () => {
+  const computedStyle = window.getComputedStyle(mobileMenu);
+  const newImagePath = "{{ site.baseurl }}/assets/icons/";
+  let closeButton = false;
+
+  if (computedStyle.display === "none") {
+    mobileMenu.style.display = "block";
+  } else {
+    mobileMenu.style.display = "none";
+  }
+
+  if (closeButton === false) {
+    menuButton.src = `${newImagePath}x-lg.svg`;
+    !closeButton;
+  } else {
+    menuButton.src = `${newImagePath}list.svg`;
+    !closeButton;
+  }
+});
