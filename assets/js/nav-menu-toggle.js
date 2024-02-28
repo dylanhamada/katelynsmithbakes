301e3e3f3465de409fcaa5e306241ec98ea9ef37
Dylan Hamada <dylan.hamada@gmail.com>
2024-02-28 12:50:04 -0500

    Prevent Page Scroll on Nav Menu Open
    - Add "noscroll" class to prevent body from scrolling when mobile nav menu open

--- assets/js/nav-menu-toggle.js
+++ assets/js/nav-menu-toggle.js
@@ -1,16 +1,18 @@
 const menuButton = document.querySelector(".nav-mobile-menu");
 const mobileMenu = document.querySelector(".nav-responsive");
+const body = document.querySelector("body");
 let closeButton = false;
 
 menuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
+  body.classList.toggle("noscroll");
 
   if (closeButton === false) {
     menuButton.classList.remove("menu-open");
-    menuButton.classList.add("menu-closed")
+    menuButton.classList.add("menu-closed");
     closeButton = true;
   } else {
-    menuButton.classList.remove("menu-closed")
+    menuButton.classList.remove("menu-closed");
     menuButton.classList.add("menu-open");
     closeButton = false;
   }
